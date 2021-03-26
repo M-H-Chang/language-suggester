297b579a54d6afa4a8fa5316d70d541d66ec2ede
Min Chang <minchangmhc@gmail.com>
2021-03-26 14:20:10 -0700

    BUGFIX: selected value returned undefined.

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,11 @@ $(document).ready(function () {
 
   $("form#questionForm").submit(function (event) {
     event.preventDefault();
-
+    const radio = $("input:radio[name=type]:checked").val();
+    const check = $("input[type='checkbox']").val();
+    const select = $("#animalSelect").val();
+    console.log(radio);
+    console.log(check);
+    console.log(select);
   })
 })
